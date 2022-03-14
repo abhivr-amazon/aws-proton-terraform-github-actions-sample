60897fa996fdfb25655b216fd639dc8af3ef122d
sbx_user1051 <sbx_user1051@169.254.102.237>
2022-03-14 17:41:53 +0000

    Update requested for Proton - component/component-11

---
+++ test_tf_env_5/component-11/terraform.tf
@@ -0,0 +1,21 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:us-east-1:101381735630:component/component-11
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+AWSTemplateFormatVersion: '2010-09-09'
+Transform: AWS::Serverless-2016-10-31
+Description: This environment holds a simple DDB table shared between services.
+Resources:
+  BasicEnvParameter:
+    Type: AWS::SSM::Parameter
+    Properties:
+      Name: test-ssm-env-param
+      Type: String
+      Value: "{{environment.outputs.vpc_arn}}"
+Outputs:
+  ParameterValue:
+    Value: !GetAtt BasicEnvParameter.Value
